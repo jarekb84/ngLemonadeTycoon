723066fc8a67c4ec6b115837fdeec7ff40602a9f
jarekb84 <jarekb@gmail.com>
2014-02-03 21:36:38 -0600

    Initial commit for ngLemonadeTycoon

---
+++ README.md
@@ -0,0 +1,2 @@
+ngLemonadeTycoon
+================
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngLemonadeTycoonApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
